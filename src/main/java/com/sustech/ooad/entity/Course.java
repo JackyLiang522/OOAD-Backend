c0635580bb2ed12a16a03c00bb9f9f40bf434015
FloweringFor <105793279+FloweringFor@users.noreply.github.com>
2022-10-22 20:54:55 +0800

    Update Course.java

--- src/main/java/com/sustech/ooad/entity/Course.java
+++ src/main/java/com/sustech/ooad/entity/Course.java
@@ -1,4 +1,47 @@
 package com.sustech.ooad.entity;
 
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import lombok.Getter;
+
+import java.util.List;
+
+@Entity
+@Getter
 public class Course {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String teacher;
+
+    private String name;
+
+    private int chapter_count;
+
+    private String introduction;
+
+    private int price;
+
+
+    // @ManyToMany(targetEntity = Client.class)
+    // @JoinTable(name = "clients")
+    @ManyToMany(mappedBy = "courses")
+    private List<Client> clients;
+
+
+    public Course() {
+
+    }
+
+    public Course(int id, String teacher, String name, int chapter_count, String introduction, int price) {
+        this.id = id;
+        this.teacher = teacher;
+        this.name = name;
+        this.chapter_count = chapter_count;
+        this.introduction = introduction;
+        this.price = price;
+    }
+
+
 }
