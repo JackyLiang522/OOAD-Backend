8bc8367f01ac2ad019b01310a772a69332b51893
FloweringFor <105793279+FloweringFor@users.noreply.github.com>
2022-10-22 20:54:25 +0800

    Update Client.java

--- src/main/java/com/sustech/ooad/entity/Client.java
+++ src/main/java/com/sustech/ooad/entity/Client.java
@@ -4,6 +4,8 @@ import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 import lombok.Getter;
 
+import java.util.List;
+
 @Entity
 @Getter
 public class Client {
@@ -17,13 +19,33 @@ public class Client {
 
     private String password;
 
+    private boolean is_student;
+
+    private boolean is_teacher;
+
+    private boolean is_admin;
+
+    @ManyToMany
+    @JoinTable(
+            name = "course_clients",
+            joinColumns = @JoinColumn(name = "client_id"),
+            inverseJoinColumns = @JoinColumn(name = "course_id"))
+    private List<Course> courses;
+
     public Client() {
     }
 
-    public Client(int id, String name, String email, String password) {
+    public Client(int id, @NotNull String name, String email, String password, boolean is_student, boolean is_teacher, boolean is_admin) {
         this.id = id;
         this.name = name;
         this.email = email;
         this.password = password;
+        this.is_student = is_student;
+        this.is_teacher = is_teacher;
+        this.is_admin = is_admin;
+    }
+
+    public String getPassword() {
+        return password;
     }
 }
