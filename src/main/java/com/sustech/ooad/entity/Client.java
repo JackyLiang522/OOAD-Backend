a5fb8ad3d579622256c32f54884ac03cb916268e
源 <73240868+JustinLiang522@users.noreply.github.com>
2022-10-22 18:13:02 +0800

    add cors

--- src/main/java/com/sustech/ooad/entity/Client.java
+++ src/main/java/com/sustech/ooad/entity/Client.java
@@ -2,8 +2,10 @@ package com.sustech.ooad.entity;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
+import lombok.Getter;
 
 @Entity
+@Getter
 public class Client {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -22,38 +24,6 @@ public class Client {
         this.id = id;
         this.name = name;
         this.email = email;
-        this.password = password;
-    }
-
-    public long getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
         this.password = password;
     }
 }
