a1b1d37e38bc9672e91a7a39d9f40450662f80f3
FloweringFor <105793279+FloweringFor@users.noreply.github.com>
2022-10-22 22:08:17 +0800

    Update Chapter.java

--- src/main/java/com/sustech/ooad/entity/Chapter.java
+++ src/main/java/com/sustech/ooad/entity/Chapter.java
@@ -1,4 +1,24 @@
 package com.sustech.ooad.entity;
 
+import javax.persistence.OneToMany;
+import java.util.ArrayList;
+import java.util.List;
+import javax.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
 public class Chapter {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @OneToMany(mappedBy = "chapter")
+    private List<Comment> comments = new ArrayList<>();
+
+
 }
+
