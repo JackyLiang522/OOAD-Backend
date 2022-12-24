e91ccb99edc043abb2d934a3b72dc29cc34cfd56
FloweringFor <105793279+FloweringFor@users.noreply.github.com>
2022-12-24 09:24:23 +0800

    Delete src directory

--- src/main/java/com/sustech/ooad/repository/CommentRepository.java
+++
@@ -1,8 +0,0 @@
-package com.sustech.ooad.repository;
-
-import com.sustech.ooad.entity.Comment;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CommentRepository extends JpaRepository<Comment, Long> {
-
-}
